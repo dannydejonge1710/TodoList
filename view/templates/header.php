44d88b5ce3f3ca6fe0474b6f5642bd12f458a36a
Danny <dannydejonge1710@outlook.com>
2019-03-12 19:09:30 +0100

    Changed Title

--- view/templates/header.php
+++ view/templates/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Studenten app</title>	
+	<title>Todo List</title>	
 	<link rel="stylesheet" href="<?= URL ?>css/style.css">
 </head>
 <body>
